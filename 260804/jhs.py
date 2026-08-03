1478bac3c1f1782f410396cd151b84dffb6b1f08
hyunsix <77480725+jhs9497@users.noreply.github.com>
2026-08-03 23:07:28 +0900

    Update jhs.py

--- 260804/jhs.py
+++ 260804/jhs.py
@@ -5,12 +5,15 @@ def solution(menu, order, k):
     for i in range(len(order)):
         order_list.append(menu[order[i]])
         people.append(len(order_list))
-        # 첫음료주문 종료시간 - 다음손님 도착시간
+        
+        # 첫음료주문 종료시간 - 다음손님 오기까지 gap시간
         order_list[0] = order_list[0] - k
-        # 다음손님이 도착했을때 음료가 여러개 나가있을수도 있으니 while문
+        
+        # 다음손님이 도착했을때 음료가 여러개 완성됐을수도 있으니 while문으로 pop
         while order_list and order_list[0] <= 0:
             if len(order_list) >= 2:
                 order_list[1] += order_list[0]
             order_list.pop(0)
+            
     answer = max(people)
     return answer
